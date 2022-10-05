01ba14959f4af7203eea3dbef9a6a3133c852e4c
nikita9954 <kunitsa.nikita2@gmail.com>
2022-10-05 14:19:20 +0300

    дз выполненное

--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -19,35 +19,75 @@ public class SmokeTest {
         driver = browsersService.getDriver();
     }
 
-    @Test
-    public void validateIKTCalculationTest() {
-        driver.get("https://clinic-cvetkov.ru/company/kalkulyator-imt/");
-        WebElement weight = driver.findElement(By.name("weight"));
-        WebElement height = driver.findElement(By.name("height"));
-        WebElement button = driver.findElement(By.id("calc-mass-c"));
 
-        height.sendKeys("183");
-        weight.sendKeys("58");
-        button.click();
-        WebElement result = driver.findElement(By.id("imt-result"));
-        Assert.assertEquals(result.getText(), "17.3 - Недостаточная (дефицит) масса тела");
-    }
     @Test
-    public void validateSKF() throws InterruptedException {
+    public void validateSmokeTest2() throws InterruptedException {
         driver.get("http://13gp.by/informatsiya/meditsinskie-kalkulyatory/995-raschet-skorosti-klubochkovoj-filtratsii-skf");
 
         WebElement selectWebElement = driver.findElement(By.id("oSex"));
         Select selectSex = new Select(selectWebElement);
-        selectSex.selectByIndex(1);
-        Thread.sleep(2000);
-        selectSex.selectByValue("0");
-        Thread.sleep(2000);
         selectSex.selectByVisibleText("женский");
-        Thread.sleep(2000);
+        WebElement ocr = driver.findElement(By.id("oCr"));
+        WebElement oage = driver.findElement(By.id("oAge"));
+        WebElement oWeight = driver.findElement(By.id("oWeight"));
+        WebElement oHeight = driver.findElement(By.id("oHeight"));
+        WebElement button = driver.findElement(By.cssSelector("#Form1 > ul > li:nth-child(6) > input[type=button]:nth-child(1)"));
+        ocr.sendKeys("80");
+        oage.sendKeys("38");
+        oWeight.sendKeys("55");
+        oHeight.sendKeys("163");
+        button.click();
+        WebElement MDRD = driver.findElement(By.id("txtMDRD"));
+        WebElement MDRD1 = driver.findElement(By.id("txtMDRD1"));
+        WebElement CG = driver.findElement(By.id("txtCG"));
+        WebElement txtBSA = driver.findElement(By.id("txtBSA"));
+        Assert.assertEquals(MDRD.getText(), "MDRD: 74 (мл/мин/1,73кв.м)");
+        Assert.assertEquals(MDRD1.getText(), "ХБП: 2 стадия (при наличии почечного повреждения)");
+        Assert.assertEquals(CG.getText(), "Cockroft-Gault: 70 (мл/мин)");
+        Assert.assertEquals(txtBSA.getText(), "Поверхность тела:1.58 (кв.м)");
     }
+
+    @Test
+    public void validateSmokeTest3() {
+        driver.get("https://kermi-fko.ru/raschety/Calc-Rehau-Solelec.aspx");
+        WebElement width = driver.findElement(By.id("el_f_width"));
+        width.sendKeys("5");
+        WebElement lenght = driver.findElement(By.id("el_f_lenght"));
+        lenght.sendKeys("15");
+        WebElement selectWebElement = driver.findElement(By.id("room_type"));
+        Select selectType = new Select(selectWebElement);
+        selectType.selectByVisibleText("Кухня или спальня");
+        WebElement selectWebElement1 = driver.findElement(By.id("heating_type"));
+        Select selectType1 = new Select(selectWebElement1);
+        selectType1.selectByVisibleText("Подогрев для комфорта");
+        WebElement losses = driver.findElement(By.id("el_f_losses"));
+        losses.sendKeys("100");
+        WebElement button = driver.findElement(By.cssSelector("#hf_calc > input"));
+        button.click();
+        WebElement power = driver.findElement(By.id("floor_cable_power"));
+        WebElement cablePower = driver.findElement(By.id("spec_floor_cable_power"));
+        Assert.assertEquals(power.getAttribute("value"), "56");
+        Assert.assertEquals(cablePower.getAttribute("value"), "1");
+    }
+
+    @Test
+    public void validateSmokeTest4() {
+        driver.get("https://calc.by/building-calculators/laminate.html");
+        driver.manage().deleteAllCookies();
+        WebElement room = driver.findElement(By.id("ln_room_id"));
+        room.sendKeys("500");
+        WebElement width = driver.findElement(By.id("wd_room_id"));
+        width.sendKeys("400");
+        WebElement longLaminat = driver.findElement(By.id("ln_lam_id"));
+        longLaminat.sendKeys("2000");
+        WebElement widhtLaminat = driver.findElement(By.id("wd_lam_id"));
+        widhtLaminat.sendKeys("200");
+    }
+
+
     @AfterMethod
-    public void tearDown()throws InterruptedException{
-        Thread.sleep(2000);
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(1000);
         driver.quit();
     }
 }
